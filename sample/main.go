7686cf41a20d20db86c0127f9e1f8f578e445a76
Hara Atsushi <toohsk@gmail.com>
2016-01-21 20:35:38 +0900

    display an empty window

---
+++ sample/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"golang.org/x/mobile/app"
+	"golang.org/x/mobile/event/lifecycle"
+	"golang.org/x/mobile/event/paint"
+)
+
+func main() {
+	app.Main(func(a app.App) {
+		for e := range a.Events() {
+			switch e := a.Filter(e).(type) {
+			case lifecycle.Event:
+				switch e.Crosses(lifecycle.StageVisible) {
+				case lifecycle.CrossOn:
+					// App visible
+				case lifecycle.CrossOff:
+					// App no longer visible.
+				}
+			case paint.Event:
+				a.Publish()
+			}
+		}
+	})
+}
